a07bd0b6fd47e88be8cab79633042ccb038ef239
Somnath <graphicsom@gmail.com>
2026-03-17 13:41:27 +0530

    upload first

---
+++ src/component/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [error, setError] = useState(false)
+    const [match, setMatch] = useState(false)
+    const [input, setInput] = useState({
+        user: '',
+        pass: ''
+    })
+      // console.log(error, "===---")
+    const checkHandler = (e)=>{
+       e.preventDefault()
+        if(input?.user !== 'user' && input?.pass !== 'password'){
+             setError(true)
+        } else {
+            setError(false)
+            setMatch(true)
+        }
+    }
+  //  console.log(error, "-------");
+    
+  return (
+    <div>
+        <h1> Login </h1>
+        
+        {error && <p className='error'> Invalid username or password </p>}
+       {match ?  <h3>Welcome User!</h3>  
+       :
+        <form onSubmit={checkHandler}>
+<label htmlFor='username' > USERNAME : 
+    <input id ="username" type='text' value={input?.user} onChange={(e)=>setInput((prev)=>({...prev, user: e.target.value}))} />
+</label>
+<label htmlFor='pass' >  PASSWOR : 
+    <input id ="pass"  type='password' value={input?.pass} onChange={(e)=>setInput((prev)=>({...prev, pass: e.target.value}))} />
+</label>
+<button> submit</button>
+        </form>
+        }
+    </div>
+  )
+}
+
+export default Login
