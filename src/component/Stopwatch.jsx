6f413ec577271e19832fda7bd25c639404f57ddc
Somnath <graphicsom@gmail.com>
2026-03-17 16:38:07 +0530

    upload stopwatch 2

--- src/component/Stopwatch.jsx
+++ src/component/Stopwatch.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 const Stopwatch = () => {
-    const [incNum, setIncNum] = useState(0)
+    const [incNum, setIncNum] = useState({min:0,sec:0})
+    
     const [run, setRun] = useState(true)
     const countref = useRef(null)
 
     const startstop = ()=>{
-
     if(run){
  countref.current = setInterval(() => {     
-        setIncNum((prev)=> prev + 1)
+        setIncNum((prev)=>({...prev, sec: prev.sec + 1}))
+        
     }, 1000);
     setRun(false)
     }   
@@ -21,10 +22,21 @@ setRun(true)
      const reset = ()=>{
 setIncNum(0)
     }
+    useEffect(()=>{
+        function setmin(){
+        if(incNum.sec > 60){
+             setIncNum((prev)=>({...prev, min: prev.min + 1}))
+             setIncNum((prev)=>({...prev, sec: 0}))
+             
+        }    
+        }
+setmin()
+    },[incNum])
 
   return (
     <div> 
-        <input type='text' value={incNum} readOnly />
+        <h2>Stop Watch</h2>
+        <p>Time: {incNum.min} : {incNum.sec}</p>
         <br/>
         <button type='button' onClick={startstop}>{run ? 'Start' : 'Stop'}</button>
         <button type='button' onClick={reset}>Reset</button>
